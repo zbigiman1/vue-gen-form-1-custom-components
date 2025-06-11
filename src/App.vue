fcb33e69fe978b9747a03de234db9c03fba4226f
Zbigniew jan Stepniewski <zbigi@zbigiman.com>
2025-06-11 17:53:57 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useForm, InputType, REGEX } from "vue-gen-form-1"
+import PasswordField from './components/PasswordField.vue'
+
+const formData = ref({
+  email: '',
+  password: '',
+  repeatPassword: '',
+})
+
+const onsubmit = () => {
+  console.log(formData.value)
+}
+
+const MyForm = useForm({
+  formData: formData,
+  legend: 'Register Form',
+  action: onsubmit,
+  submitButtonText: 'Submit',
+  fields:
+    [
+      {
+        name: 'email',
+        label: 'Email',
+        type: InputType.EMAIL,
+        component: 'input',
+        modelValue: ref(formData.value.email),
+        validation: [
+          {
+            role: 'required',
+            message: 'The email is required'
+          },
+          {
+            role: REGEX.EMAIL,
+            message: 'Please enter the valid email.'
+          }]
+      },
+      {
+        name: 'password',
+        label: 'Password',
+        type: InputType.PASSWORD,
+        component: PasswordField,
+        modelValue: ref(formData.value.password),
+        validation: [{
+          role: 'required',
+          message: 'Password is required'
+        }, {
+          role: REGEX.MIN_LENGTH(8),
+          message: 'The password should be at least 8 characters long.'
+        },
+        {
+          role: REGEX.LOWER_AND_UPPER_CASE,
+          message: 'The password should contain at least one lowercase and one uppercase character.'
+        },
+        {
+          role: REGEX.AT_LEAST_ONE_SPECIAL_CHARACTER,
+          message: 'The password should contain at least one special character.'
+        },
+        {
+          role: REGEX.AT_LEAST_ONE_NUMBER,
+          message: 'The password should contain at least one number.'
+        }]
+      },
+      {
+        name: 'repeatPassword',
+        label: 'Repeat password',
+        type: InputType.PASSWORD,
+        component: PasswordField,
+        modelValue: ref(formData.value.repeatPassword),
+        validation: [{
+          role: 'required',
+          message: 'The repeat password is required.'
+        },
+        {
+          role: () => {
+            return formData.value.password === formData.value.repeatPassword
+          },
+          message: 'Passwords do not match.'
+        }]
+      },      
+    ]
+})
+</script>
+
+<template>
+  <main class="flex gap-10">
+    <MyForm class="w-1/2" />
+    <pre class="w-1/2 p-4 bg-gray-800 text-gray-200">{{ formData }}</pre>
+  </main>
+</template>
---
+++ src/components/PasswordField.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="form__field">
+        <label class="form__field__label" :for="name">{{ label }}</label>
+        <div class="w-full">
+            <input class="form__field__input w-full" :name="name" :type="isShowPassword ? 'text' : type"
+                v-model="modelValue" @blur="onBlur">
+            <button type="button" @click="showPassword" class="absolute -ml-8 mt-3 cursor-pointer">
+                <template v-if="isShowPassword">
+                    <img src="/eye-slash.svg" alt="hide" class="w-5" />
+                </template>
+                <template v-else>
+                    <img src="/eye.svg" alt="show" class="w-5" />
+                </template>
+            </button>
+        </div>
+        <div class="form__field__errors">
+            <template v-for="error in errors">
+                <div class="form__field__errors__item">{{ error }}</div>
+            </template>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, Ref, toRefs, watch } from 'vue'
+
+const props = defineProps<{
+    name: string
+    label: string
+    modelValue: any
+    type: string
+    value?: any
+    errors: Ref<string[]>
+    pristine: Ref<boolean>
+}>()
+
+const { modelValue, type, errors, pristine } = toRefs(props)
+const emit = defineEmits(['update:modelValue'])
+
+const isShowPassword = ref(false)
+
+function showPassword() {
+    isShowPassword.value = !isShowPassword.value
+}
+
+function onBlur() {
+    pristine.value = false
+    emit('update:modelValue', modelValue.value)
+}
+
+watch(modelValue, (value) => {
+    emit('update:modelValue', value)
+})
+</script>
+
+<style scoped></style>
